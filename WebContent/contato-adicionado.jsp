1640cd4c8fd38d614012853450ec205d7f39edbc
vinicius-guima <vinicius_guima.social@hotmail.com>
2018-12-18 21:44:25 -0200

    começo cap 9

---
+++ WebContent/contato-adicionado.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=utf-8"
+    pageEncoding="utf-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>contato-adicionado</title>
+</head>
+<body>
+
+Contato ${param.nome} adicionado com sucesso!
+
+</body>
+</html>
--- src/br/com/caelum/servlet/AdicionaContatoServlet.java
+++ src/br/com/caelum/servlet/AdicionaContatoServlet.java
@@ -7,6 +7,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -50,11 +51,8 @@ public class AdicionaContatoServlet extends HttpServlet {
 		ContatoDao dao = new ContatoDao();
 		dao.adiciona(contato);
 		
-		out.println("<html>");
-		out.println("<body>");
-		out.println("Contato " + contato.getNome() + " adicionado com sucesso!!!");
-		out.println("</body>");
-		out.println("</html>");
+		RequestDispatcher rd = request.getRequestDispatcher("contato-adicionado.jsp");
+		rd.forward(request, response);
 	
 	}
 
